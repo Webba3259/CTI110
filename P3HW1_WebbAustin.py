938709ec2b3ffcec2a5f28b611d529e1d8e62061
Webba3259 <99507784+Webba3259@users.noreply.github.com>
2022-03-13 15:28:24 -0400

    TTL
    Took too long to remember that i had to float the input to get the if statements to accept it

---
+++ P3HW1_WebbAustin.py
@@ -0,0 +1,276 @@
+Python 3.10.2 (tags/v3.10.2:a58ebcc, Jan 17 2022, 14:12:15) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for3
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 5
+how many students will be sharing each pizza? (1.5, 2, or 3)5
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    if not value: 1.5, 2, 3
+NameError: name 'value' is not defined. Did you mean: 'False'?
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)5
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)1.5
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 4, in <module>
+    round(nos / psp)
+TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 56
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)2
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+----Pizza Order--------
+Number of Students : 56
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)2
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)1.5
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)2
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)2
+----Pizza Order--------
+Number of Students : 56
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    pn = round(nos / psp) #pizzas needed
+TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    pn = (nos / psp) #pizzas needed before rounding
+TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    pn = ('nos'/'psp') #pizzas needed before rounding
+TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    pn = float(nos/psp) #pizzas needed before rounding
+TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    pn = nos//psp #pizzas needed before rounding
+TypeError: unsupported operand type(s) for //: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+Traceback (most recent call last):
+  File "C:/Users/Austin/Documents/P3HW2_WebbAustin.py", line 3, in <module>
+    pn = nos/psp #pizzas needed before rounding
+TypeError: unsupported operand type(s) for /: 'str' and 'str'
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 56.0
+Pizzas Needed : 18.666666666666668
+price $ 100.7
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 56
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 56.0
+Pizzas Needed : 19
+price $ 100.7
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 18
+price $ 95.4
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 18
+price $ 95.4
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 18
+price $ 95.4
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 18
+price $ 95.4
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 18
+price $ 95.4
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 19
+price $ 100.7
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)3
+----Pizza Order--------
+Number of Students : 55.0
+Pizzas Needed : 19
+price $ 100.7
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 55
+how many students will be sharing each pizza? (1.5, 2, or 3)5
+INVALID ENTRY!!!!
+should have entered 1.5, 2, or 3
+
+Run Program Again
+
+============ RESTART: C:/Users/Austin/Documents/P3HW2_WebbAustin.py ============
+number of students you are ordering for? 27
+how many students will be sharing each pizza? (1.5, 2, or 3)2
+----Pizza Order--------
+Number of Students : 27.0
+Pizzas Needed : 14
+price $ 74.2
+--------------------------
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 80
+Traceback (most recent call last):
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 36, in <module>
+    main()
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 17, in main
+    if score >= A_score:
+TypeError: '>=' not supported between instances of 'str' and 'int'
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 60
+Traceback (most recent call last):
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 39, in <module>
+    main()
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 20, in main
+    if score >= A_score:
+TypeError: '>=' not supported between instances of 'str' and 'int'
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 95
+Traceback (most recent call last):
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 39, in <module>
+    main()
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 20, in main
+    if score >= A_score:
+TypeError: '>=' not supported between instances of 'str' and 'float'
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 95
+Traceback (most recent call last):
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 39, in <module>
+    main()
+  File "C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py", line 20, in main
+    if score >= A_score:
+TypeError: '>=' not supported between instances of 'str' and 'int'
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 95
+Your grade is: A
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 40
+Your grade is: F
+
+==== RESTART: C:\Users\Austin\Downloads\P3HW1_Debugging_lastname _1_ (1).py ====
+Enter grade: 85
+Your grade is: B
